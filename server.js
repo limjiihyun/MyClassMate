17f27e06cb5f18e1ae2e977e46cddb653fce4fce
limjiihuun <dlawlgus40@naver.com>
2023-05-01 17:08:38 +0900

    5/1

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true})) 
 const MongoClient = require('mongodb').MongoClient;
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
 app.set('view engin', 'ejs');
+
+app.use('/public', express.static('public'));
+//미들웨어, 요청과응답 사이에 적는 것, 나는 static파일을 보관하기 위해 public를 사용한다.
 
 var db;
 MongoClient.connect('mongodb+srv://new-project:dlawlgus55@cluster0.yft1dyh.mongodb.net/?retryWrites=true&w=majority', 
@@ -24,7 +30,7 @@ MongoClient.connect('mongodb+srv://new-project:dlawlgus55@cluster0.yft1dyh.mongo
 })
 
 app.get('/', function(요청, 응답) { 
-    응답.sendFile(__dirname+'/index.html') 
+    응답.render('index.ejs');
 });
 
 app.post('/add', function(요청, 응답){
@@ -43,8 +49,8 @@ app.post('/add', function(요청, 응답){
     
 });
 
-app.get('/board', function(요청, 응답) { 
-    응답.sendFile(__dirname+'/board.html') 
+app.get('/write', function(요청, 응답) { 
+    응답.render('write.ejs');
 });
 
 app.get('/list', function(요청, 응답){
@@ -69,3 +75,19 @@ app.get('/detail/:id', function(요청, 응답){
         응답.render('detail.ejs', { data : 결과})
     })
 })
+
+app.get('/edit/:id', function(요청, 응답){
+    db.collection('post').findOne({_id: parseInt(요청.params.id)}, function(에러, 결과){
+        console.log(결과)
+        응답.render('edit.ejs', { post : 결과 })
+    })
+})
+
+app.put('/edit', function(요청, 응답){ 
+    db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 
+      요청.body.title , 날짜 : 요청.body.date }}, 
+      function(에러, 결과){ 
+      console.log('수정완료') 
+      응답.redirect('/list') 
+    }); 
+  }); 
